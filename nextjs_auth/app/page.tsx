77778e0dc54cf69ab1f2a2810bccc29e3afa9a3b
Enderz420 <40892919+Enderz420@users.noreply.github.com>
2025-03-06 19:15:17 +0100

    tidy up landing page

--- nextjs_auth/app/page.tsx
+++ nextjs_auth/app/page.tsx
@@ -2,14 +2,14 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div className="h-screen flex items-center justify-center flex-col bg-[radial-gradient(60%_120%_at_50%_50%,hsla(0,0%,100%,0)_0,rgba(252,205,238,.5)_100%)]">
-      <h1 className="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl">
-        Home
-      </h1>
-      <div className="grid gap-4 col-span-2 mt-10">
-        <Button className="rounded-xl">Klikk meg!</Button>
-        <Button className="rounded-xl">Klikk meg også!</Button>
+    <main className="flex items-center justify-center flex-col min-h-screen bg-gradient-to-br text-white from-slate-900 to-slate-800">
+      <div className="flex flex-col gap-y-10">
+        <h1 className="text-8xl">Authentication</h1>
+        <div className="flex flex-col items-center gap-y-10">
+          <Button variant="secondary">Go to login!</Button>
+          <Button variant="secondary">Go to register!</Button>
+        </div>
       </div>
-    </div>
+    </main>
   );
 }
