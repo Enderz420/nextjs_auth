ab3e3d46c774dff181f56584e0631172d0b81d9b
Enderz420 <40892919+Enderz420@users.noreply.github.com>
2025-03-09 01:48:41 +0100

    fix navbar using w-screen instead of w-full, remove button from navbar change styling of footer and navbar

--- nextjs_auth/components/footer.tsx
+++ nextjs_auth/components/footer.tsx
@@ -2,10 +2,8 @@ import React from "react";
 
 function Footer() {
   return (
-    <footer className="w-full text-center text-white bg-slate-700 p-2 bottom-0">
-      <div className="mx-5">
-        <h3>Laget med ❤️</h3>
-      </div>
+    <footer className="w-full text-center text-white bg-slate-950 p-6 ">
+      <h3>Laget med ❤️</h3>
     </footer>
   );
 }
--- nextjs_auth/components/navbar.tsx
+++ nextjs_auth/components/navbar.tsx
@@ -4,27 +4,19 @@ import { Button } from "./ui/button";
 
 function Navbar() {
   return (
-    <header className="bg-slate-700 text-white">
-      <nav className="flex justify-evenly w-screen py-2">
-        <div>
-          <Link className="mx-2 outline-gray-500" href="/">
-            <Button variant="default" className="text-white">
-              Home
-            </Button>
-          </Link>
-          <Link className="mx-2 outline-gray-500" href="/auth/register">
-            <Button variant="default" className="text-white">
-              Register
-            </Button>
-          </Link>
-          <Link className="mx-2 outline-gray-500" href="/auth/login">
-            <Button variant="default" className="text-white">
-              Login
-            </Button>
-          </Link>
-        </div>
-      </nav>
-    </header>
+    <nav className="flex justify-evenly w-full py-2 bg-slate-950 text-white">
+      <div>
+        <Link className="mx-2 outline-gray-500" href="/">
+          Home
+        </Link>
+        <Link className="mx-2 outline-gray-500" href="/auth/register">
+          Register
+        </Link>
+        <Link className="mx-2 outline-gray-500" href="/auth/login">
+          Login
+        </Link>
+      </div>
+    </nav>
   );
 }
 
