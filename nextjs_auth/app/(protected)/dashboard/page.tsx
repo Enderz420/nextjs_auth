fac015b3f79c9eb195102f65348beeaba8bf1d3f
Enderz420 <40892919+Enderz420@users.noreply.github.com>
2025-03-09 01:51:54 +0100

    complete part 1 of 3... (god help)

---
+++ nextjs_auth/app/(protected)/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Dashboard() {
+  return (
+    <main className="flex items-center justify-center flex-col min-h-screen bg-gradient-to-br text-white from-slate-900 to-slate-800">
+      <div>secret!!!!</div>
+    </main>
+  );
+}
+
+export default Dashboard;
