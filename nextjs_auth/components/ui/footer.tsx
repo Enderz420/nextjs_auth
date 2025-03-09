08594828775f1db1f159dd531f58a3a65e45bee6
Enderz420 <40892919+Enderz420@users.noreply.github.com>
2025-03-09 01:30:54 +0100

    create navbar and footer

---
+++ nextjs_auth/components/ui/footer.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="w-full text-center text-white bg-slate-700 p-2 bottom-0">
+      <div className="mx-5">
+        <h3>Laget med ❤️</h3>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ nextjs_auth/components/ui/navmenu.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Link from "next/link";
+import { Button } from "./button";
+
+function Navmenu() {
+  return (
+    <header className="bg-slate-700 text-white p-2">
+      <nav>
+        <div className="flex">
+          <Link className="mx-2 outline-gray-500" href="/auth/register">
+            <Button variant="default" className="text-white">
+              Register
+            </Button>
+          </Link>
+          <Link className="mx-2 outline-gray-500" href="/auth/login">
+            <Button variant="default" className="text-white">
+              Login
+            </Button>
+          </Link>
+        </div>
+      </nav>
+    </header>
+  );
+}
+
+export default Navmenu;
